a29142260fa5ac8c087ab085df9175d0607c5999
Emil Babayev <zentabit@gmail.com>
2023-02-24 22:49:07 +0100

    I tried making a thing with k_f and k_v

---
+++ emils test/threedecay.py
@@ -0,0 +1,48 @@
+import numpy as np
+import lmfit
+import corner
+import matplotlib.pyplot as plt
+
+BR_sm_ZZ = 0.0266700
+BR_sm_bb = 0.5792000
+BR_sm_WW = 0.2170000
+
+mu_ww = [2.1, 1]
+unc_ww = [1.4, 0.5]
+
+mu_zz = [2.1,0.8]
+unc_zz = [1.4, 0.46]
+
+mu_bb = [1.5,0.7]
+unc_bb = [1.1,1.9]
+
+def residue(params):
+    k_f = params['k_f']
+    k_v = params['k_v']
+    BR_inv = params['BR_inv']
+
+    mu_model_ww = (k_f**2 * k_v**2* (1-BR_inv))/((BR_sm_WW+ BR_sm_ZZ)*k_v**2 + BR_sm_bb*k_f**2)
+    mu_model_zz = (k_f**2 * k_v**2* (1-BR_inv))/((BR_sm_WW+ BR_sm_ZZ)*k_v**2 + BR_sm_bb*k_f**2)
+    mu_model_bb = (k_f**2 * k_f**2* (1-BR_inv))/((BR_sm_WW+ BR_sm_ZZ)*k_v**2 + BR_sm_bb*k_f**2)
+    
+    res_ww = (mu_model_ww - mu_ww)/unc_ww
+    res_zz = (mu_model_zz - mu_zz)/unc_zz
+    res_bb = (mu_model_bb - mu_bb)/unc_bb
+
+    return np.hstack((res_ww, res_zz, res_bb))
+
+#Skapa parametrar och initiella värden
+par = lmfit.Parameters()
+par.add('k_f',1, min = -5, max = 5)
+par.add('k_v',1, min = -5, max = 5)
+par.add('BR_inv', 0, min = 0, max = 1)
+
+out = lmfit.minimize(residue, par,method = 'leastsq')
+
+# write error report
+lmfit.report_fit(out)
+
+#Corner plot
+bay = lmfit.minimize(residue, method='emcee', nan_policy='omit', burn=300, steps=5000, thin=20,params=out.params, is_weighted=True, progress=True)
+emcee_plot = corner.corner(bay.flatchain, labels=bay.var_names,truths=list(bay.params.valuesdict().values()))
+plt.show()
