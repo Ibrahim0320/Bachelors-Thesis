fc08bb01a55ea9b0cca1d05eddce28297f97d132
Emil Babayev <zentabit@gmail.com>
2023-03-13 14:53:08 +0100

    first work on the singlet scalar BSM model

--- prettyplot.py
+++ prettyplot.py
@@ -7,8 +7,11 @@ run = "2023-03-08 17:56:35.578213"
 
 var_names = ['k_w', 'k_z','k_b','k_t','k_mu','k_tau','k_gg','k_gamgam']
 flatchain = pandas.read_csv(f"flatchains/flatchain_{run}.csv", sep=",", names=var_names)
-truths = pandas.read_csv(f"truths/truth_2023-03-08 17:56:36.130561.csv", names=var_names)
+truths = np.loadtxt(f"truths/truth_2023-03-08 17:56:36.130561.csv",delimiter=",")
 
-emcee_plot = corner.corner(flatchain, labels=var_names, levels = (0.69,0.95),bins=50,smooth=True,truths=list(truths))
+
+emcee_plot = corner.corner(flatchain, labels=var_names, levels = (0.69,0.95,),bins=50,smooth=True,truths=truths[:-1],verbose=True, plot_datapoints=False,quantiles=(0.16, 0.84))
+
+
 
 plt.show()
