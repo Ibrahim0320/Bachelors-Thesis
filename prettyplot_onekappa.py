6c088056580f7a3f9e1e6a4ec481b37efdb2be4b
Emil Babayev <zentabit@gmail.com>
2023-03-24 10:33:35 +0100

    moar plots

--- prettyplot_onekappa.py
+++ prettyplot_onekappa.py
@@ -7,18 +7,18 @@ run = "2023-03-21_2122"
 
 # var_names = ['k_v','k_b','k_t','k_mu','k_tau','k_gg','k_gamgam', 'BR_inv']
 # var_names = ['k_w','k_z','k_b','k_t','k_mu','k_tau','k_gg','k_gamgam']
-var_names = ['k_v','k_f']
+var_names = ['k']
 
-SM_truth = [1,1]
+SM_truth = [1]
 # SM_truth = [1, 1, 1, 1, 1, 1, 0, 0]
 SM_points = np.array([SM_truth, SM_truth])
 labels = [
-    r"$\kappa_v$",
+    r"$\kappa$",
     # r"$\cos(\theta)$",
     # r"$1-BR_{inv}$"
     # r"$\kappa_w$",
     # r"$\kappa_z$",
-    r"$\kappa_f$",
+    # r"$\kappa_f$",
 ]
 
 flatchain = pandas.read_csv(f"flatchains/flatchain_{run}.csv", sep=",")[var_names]
@@ -31,11 +31,11 @@ emcee_plot = corner.corner(flatchain,
                            levels = (0.68,0.95,), 
                            bins=50, 
                            smooth=True, 
-                           truths=truths[0:2], 
+                           truths=[truths[0]], 
                            verbose=True, 
                            plot_datapoints=False, 
-                           quantiles=(0.05,0.95), 
-                        #    title_quantiles=(0.05,0.95),
+                           quantiles=(0.025,0.975), 
+                        #    title_quantiles=(0.025,0.5,0.975),
                         #    show_titles=True
                            )
 
---
+++ prettyplot_twokappas.py
@@ -0,0 +1,44 @@
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+import corner
+
+run = "2023-03-21_2122"
+
+# var_names = ['k_v','k_b','k_t','k_mu','k_tau','k_gg','k_gamgam', 'BR_inv']
+# var_names = ['k_w','k_z','k_b','k_t','k_mu','k_tau','k_gg','k_gamgam']
+var_names = ['k_v','k_f']
+
+SM_truth = [1,1]
+# SM_truth = [1, 1, 1, 1, 1, 1, 0, 0]
+SM_points = np.array([SM_truth, SM_truth])
+labels = [
+    r"$\kappa_v$",
+    # r"$\cos(\theta)$",
+    # r"$1-BR_{inv}$"
+    # r"$\kappa_w$",
+    # r"$\kappa_z$",
+    r"$\kappa_f$",
+]
+
+flatchain = pandas.read_csv(f"flatchains/flatchain_{run}.csv", sep=",")[var_names]
+truths = np.loadtxt(f"truths/truth_{run}.csv", delimiter=",")
+
+# print(flatchain)
+
+emcee_plot = corner.corner(flatchain, 
+                           labels=labels, 
+                           levels = (0.68,0.95,), 
+                           bins=50, 
+                           smooth=True, 
+                           truths=truths[0:2], 
+                           verbose=True, 
+                           plot_datapoints=False, 
+                           quantiles=(0.025,0.975), 
+                        #    title_quantiles=(0.025,0.5,0.975),
+                        #    show_titles=True
+                           )
+
+# corner.overplot_lines(emcee_plot, SM_truth, color = 'C1')
+corner.overplot_points(emcee_plot, SM_points, marker = '*', color = 'orangered')
+plt.show()
