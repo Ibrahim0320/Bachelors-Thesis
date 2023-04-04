4960bc7c3ee1b962c04f90ed9c6660f659e86184
Emil Babayev <zentabit@gmail.com>
2023-04-04 10:19:19 +0200

    prettier plots for singscal and HPDM

--- prettyplot_onekappa.py
+++ prettyplot_onekappa.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import corner
 
-run = "2023-03-21_2122"
+run = "2023-03-21_2003"
 
 # var_names = ['k_v','k_b','k_t','k_mu','k_tau','k_gg','k_gamgam', 'BR_inv']
 # var_names = ['k_w','k_z','k_b','k_t','k_mu','k_tau','k_gg','k_gamgam']
@@ -13,8 +13,8 @@ SM_truth = [1]
 # SM_truth = [1, 1, 1, 1, 1, 1, 0, 0]
 SM_points = np.array([SM_truth, SM_truth])
 labels = [
-    r"$\kappa$",
-    # r"$\cos(\theta)$",
+    # r"$\kappa$",
+    r"$\cos(\alpha)$",
     # r"$1-BR_{inv}$"
     # r"$\kappa_w$",
     # r"$\kappa_z$",
@@ -34,11 +34,15 @@ emcee_plot = corner.corner(flatchain,
                            truths=[truths[0]], 
                            verbose=True, 
                            plot_datapoints=False, 
-                           quantiles=(0.025,0.975), 
+                           quantiles=(0.05,), 
                         #    title_quantiles=(0.025,0.5,0.975),
                         #    show_titles=True
                            )
-
+ax = emcee_plot.get_axes()[0]
 # corner.overplot_lines(emcee_plot, SM_truth, color = 'C1')
 corner.overplot_points(emcee_plot, SM_points, marker = '*', color = 'orangered')
+
+sec = ax.secondary_xaxis('top', functions=(np.square, np.sqrt))
+sec.set_xlabel(r"$1-BR_{inv}$")
+
 plt.show()
